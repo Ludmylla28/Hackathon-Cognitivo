a2b6b94a26e9bfbf0e86f490b4a5b0965d463ad7
Ludmylla Bianca <you@example.com>
2018-03-17 21:11:34 -0300

    atualização

--- Vision/public/js/bundle.js
+++
@@ -1,7 +0,0 @@
-// note: this file is not served as-is by express static,
-// it is run through browserify and the resulting bundle is served by express-browserify
-
-require('./demo.js');
-require('./use.js');
-require('./train.js');
-require('./errormsg.jsx');
